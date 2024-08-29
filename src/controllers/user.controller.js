5e5f3271125b50ab9ab12fc61ad3bb415b8ac06a
sohaib rafique <sohaibrafiq991@gmail.com>
2024-08-29 22:21:50 +0500

    add new routes for user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudnary } from "../utils/cloudinary.js";
 import { ApiResponce } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import { mongo } from "mongoose";
 
 const generateAccessAndRefereshTokens = async (userId) => {
   try {
@@ -339,15 +340,54 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
       }
      }
     ])
+
+    if(!channel?.length){
+      throw new ApiError(404,"channel does not exist ")
+    }
+    return res 
+    .status(200)
+    .json(
+      new ApiResponce(200,channel[0],"user channel fetched successfully")
+    )
 })
-if(!channel?.length){
-  throw new ApiError(404,"channel does not exist ")
-}
-return res 
-.status(200)
-.json(
-  new ApiResponce(200,channel[0],"user channel fetched successfully")
-)
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+  const user = await User.aggregate([
+      {
+        $match:{
+          _id: new mongoose.Types.ObjectId(req.user._id)
+        }
+      },
+      {
+        $lookup:{
+          from:"videos",
+          localField:"watchHistory",
+          foreignField:"_id",
+          as:"watch History",
+          pipeline:[
+            {
+              $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as:"owner",
+                pipeline:[
+                  {
+                    $project:{
+                      fullName:1,
+                      username:1,
+                      avatar:1
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+  ])
+})
+
 export {
   registerUser,
   loginUser,
@@ -358,5 +398,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWatchHistory
 };
